3872a963570acc8acb529898735695cc7bd66ea0
Nawin chander <nawinnapzy289@gmail.com>
2026-03-06 20:24:06 +0530

    add code

--- script2.js
+++ script2.js
@@ -11,3 +11,45 @@ function findMax(root) {
 
 console.log(findMax(root));
 
+/// op
+
+// 5
+
+
+////Count Number of Nodes
+
+function countNodes(root) {
+  if (root === null) return 0;
+
+  return 1 + countNodes(root.left) + countNodes(root.right);
+}
+
+console.log(countNodes(root));
+
+
+/// op
+
+// 5
+
+
+/// Find Height of Tree
+
+function height(root) {
+  if (root === null) return -1;
+
+  let left = height(root.left);
+  let right = height(root.right);
+
+  return Math.max(left, right) + 1;
+}
+
+console.log(height(root));
+
+/// op 
+
+/// 2
+
+
+
+
+
