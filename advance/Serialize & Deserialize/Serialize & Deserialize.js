d5a300195aeb576b98c15649d7fca7b49dfd72dc
Nawin chander <nawinnapzy289@gmail.com>
2026-03-22 08:42:14 +0530

    add the file

---
+++ advance/Serialize & Deserialize/Serialize & Deserialize.js
@@ -0,0 +1,14 @@
+// Serialize & Deserialize Binary Tree
+
+// Problem: Convert a binary tree into a string and back.
+
+
+// Use Preorder traversal
+// Represent null nodes as "null"
+// Use a queue to rebuild tree
+
+
+
+
+
+
