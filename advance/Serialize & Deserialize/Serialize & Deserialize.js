ae940c298d732b3a980e31bbd5da268e764ea958
Nawin chander <nawinnapzy289@gmail.com>
2026-03-22 08:42:37 +0530

    add code

--- advance/Serialize & Deserialize/Serialize & Deserialize.js
+++ advance/Serialize & Deserialize/Serialize & Deserialize.js
@@ -8,6 +8,45 @@
 // Use a queue to rebuild tree
 
 
+function serialize(root) {
+    let result = [];
+
+    function dfs(node) {
+        if (!node) {
+            result.push("null");
+            return;
+        }
+        result.push(node.val);
+        dfs(node.left);
+        dfs(node.right);
+    }
+
+    dfs(root);
+    return result.join(",");
+}
+
+function deserialize(data) {
+    let list = data.split(",");
+
+    function buildTree() {
+        if (list[0] === "null") {
+            list.shift();
+            return null;
+        }
+
+        let node = new TreeNode(parseInt(list[0]));
+        list.shift();
+
+        node.left = buildTree();
+        node.right = buildTree();
+
+        return node;
+    }
+
+    return buildTree();
+}
+
+
 
 
 
